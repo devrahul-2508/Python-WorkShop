c4ad735e85c845e7269f75c33fffc71e4d481756
Nirmalya-Saha96 <sahanirmalyo96@gmail.com>
2023-01-13 12:36:36 +0530

    Add Data Analysis

--- Python/DataAnalysis/analysis.py
+++ Python/DataAnalysis/analysis.py
@@ -2,13 +2,29 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('data.csv')
-print(dataset.head(10))
+def ploting(dataset, key):
+    count = dataset[key].value_counts()
+    values = count.values
+    label = count.index
 
+    fig = plt.figure(figsize=(10,7))
+    plt.pie(values, labels=label)
+    plt.show()
+
+dataset = pd.read_csv('data.csv')
+
+runs = dataset.groupby('Pos')['Runs'].sum()
 total_runs = dataset['Runs'].sum()
 total_matches = len(dataset['Runs'])
 average_runs = dataset['Runs'].mean()
 position = dataset['Pos'].unique()
+
+total_sizes = dataset.groupby('Opposition')['6s'].sum()
+
+centuries = dataset.query("Runs >= 100")
+innings = centuries["Inns"]
+tons = centuries['Runs']
+
 
 dataset['Pos'] = dataset['Pos'].map({
     3.0: "Batting at 3",
@@ -23,13 +39,37 @@ dataset['Pos'] = dataset['Pos'].map({
 position_count = dataset["Pos"].value_counts()
 position_values = position_count.values
 
-fig = plt.figure(figsize=(10,7))
-plt.show()
-
 print("Total number of runs: ", total_runs)
 print("Total matches played: ", total_matches)
 print("Average Runs: ", int(average_runs))
 print("Positions played in: ", position)
 print("Positions frequency: ", position_count)
-print(position_values)
+print("Positions values: ", position_values)
 print("Dataset: \n", dataset)
+print("Runs: ", total_sizes)
+print("Centuries Dataset \n", centuries)
+
+ploting(dataset, "Pos")
+ploting(dataset, 'Opposition')
+ploting(dataset, 'Ground')
+ploting(dataset, 'Dismissal')
+
+runs_values = runs.values
+runs_labels = runs.index
+fig = plt.figure(figsize=(10,7))
+plt.pie(runs_values, labels=runs_labels)
+plt.show()
+
+six_values =  total_sizes.values
+six_labels =  total_sizes.index
+fig = plt.figure(figsize=(10,7))
+plt.pie( six_values, labels= six_labels)
+plt.show()
+
+fig = plt.figure(figsize=(10,7))
+plt.bar(innings, tons, color='yellow', width=0.2)
+plt.show()
+
+fig = plt.figure(figsize=(10,7))
+plt.bar(centuries['Opposition'], centuries["Runs"], color='yellow', width=0.2)
+plt.show()
