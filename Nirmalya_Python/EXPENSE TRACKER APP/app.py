c7cc9f932928b5fa7e2b9904367a6789363e4d76
Nirmalya-Saha96 <sahanirmalyo96@gmail.com>
2023-01-19 11:43:02 +0530

    Add Routes

--- Nirmalya_Python/EXPENSE TRACKER APP/app.py
+++ Nirmalya_Python/EXPENSE TRACKER APP/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from uuid import uuid1, uuid4
 import os, json, pytz
 from datetime import datetime, date
+import pandas as pd
 
 db = {}
 db_filename = "db.json"
@@ -179,6 +180,106 @@ def addPurchase():
             "error_code": 500,
             "error_message": "Error: Trying to access endpoint with wrong method"
         }      
+
+#Get all purchases for today
+@app.route("/get_all_purchases_for_today", methods=["GET"])
+def get_all_purchases_for_today():
+    user_idx = int(request.args['user_index'])
+    curr_date = str(date.today())
+    purchase_dates = list(db['users'][user_idx]['purchase'].keys())
+
+    if(user_idx > len(db['users'])):
+        return {
+            "status": "fail",
+            "code": "404",
+            "data": "user index not found"
+        }
+    elif(curr_date in purchase_dates):
+        return {
+            "status": "success",
+            "code": "200",
+            "data": db['users'][user_idx]['purchase'][curr_date]
+        }
+    else: 
+        return {
+            "status": "success",
+            "code": "200",
+            "data": "No records found"
+        }
+
+#Get all purchases for time duration in params
+@app.route("/get_purchases_time_duration", methods=["GET"])
+def get_purchases_time_duration():
+    user_idx = int(request.args['user_index'])
+    start_date = request.args['start_date']
+    end_date = request.args['end_date']
+    response = {}
+    keys = list(db['users'][user_idx]['purchase'].keys())
+    flag = False
+    date_range = pd.date_range(start_date, end_date)
+
+    if(len(db['users']) < user_idx):
+        return {
+            "status": "fail",
+            "code": "404",
+            "data": "User not found"
+        }
+
+    for element in keys:
+        if(element in date_range):
+            flag = True
+            response[element] = db['users'][user_idx]['purchase'][element]
+
+    if(flag):
+        return {
+            "status": "success",
+            "code": "200",
+            "data": response
+        }
+    else: 
+        return {
+            "status": "success",
+            "code": "200",
+            "data": "No records found"
+        }
+
+#Get all purchases for time duration in body raw json
+@app.route("/get_purchases", methods=["GET"])
+def get_purchases():
+    data = request.json
+    user_idx = data['user_index']
+    start_date = data['start_date']
+    end_date = data['end_date']
+    response = {}
+    keys = list(db['users'][user_idx]['purchase'].keys())
+    flag = False
+    date_range = pd.date_range(start_date, end_date)
+
+    if(len(db['users']) < user_idx):
+        return {
+            "status": "fail",
+            "code": "404",
+            "data": "User not found"
+        }
+
+    for element in keys:
+        if(element in date_range):
+            flag = True
+            response[element] = db['users'][user_idx]['purchase'][element]
+
+    if(flag):
+        return {
+            "status": "success",
+            "code": "200",
+            "data": response
+        }
+    else: 
+        return {
+            "status": "success",
+            "code": "200",
+            "data": "No records found"
+        }
+
 
 
 
